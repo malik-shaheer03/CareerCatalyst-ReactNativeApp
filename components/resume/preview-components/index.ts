1eb01c186a15e2b34180c2eace7d9d6985e1cc9d
Muhammad Shaheer Malik <shaheermalik03@gmail.com>
2025-10-11 11:09:20 +0500

    Removed unused Imports

--- components/resume/preview-components/index.ts
+++
@@ -1,9 +0,0 @@
-// Preview Components Export
-export { default as ResumePreview } from './ResumePreview';
-export { default as CompactResumePreview } from './CompactResumePreview';
-export { default as ResumePDFExport } from './ResumePDFExport';
-
-// Type exports
-export type { ResumePreviewProps } from './ResumePreview';
-export type { CompactResumePreviewProps } from './CompactResumePreview';
-export type { ResumePDFExportProps } from './ResumePDFExport';
